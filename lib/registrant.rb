76128131c37ee436cb6e06dd38b4a7c83a3a1e08
Jim Macur <jim.macur@gmail.com>
2024-07-11 15:42:49 -0600

    adds it blocks to registrant_spec

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,3 +1,28 @@
+
 class Registrant 
-    
+    attr_reader    :name, 
+                    :age, 
+                    :permit
+
+    def initialize(name, age, permit = false)
+        @name = name
+        @age = age
+        @permit = permit
+    end
+
+    def permit?
+        @permit
+    end
+
+   def license_data
+        {
+        written: false,
+        license: false,
+        renewed: false
+        }
+    end
+
+    def earn_permit
+        @permit = true
+    end
 end
