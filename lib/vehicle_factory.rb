385c699ce655ee6e94f385ef5e859180640517c7
Jim Macur <jim.macur@gmail.com>
2024-07-13 17:17:59 -0600

    Completes Vehicle factory test and code

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,5 +1,17 @@
 class VehicleFactory
-  def initialize
-    true
-  end
+    
+    def initialize 
+    end
+
+    def create_vehicles(dmv_registrations)
+        dmv_registrations.map do |registration|
+          vehicle_data = {}
+          vehicle_data[:vin] = registration[:vin_1_10]
+          vehicle_data[:year] = registration[:model_year]
+          vehicle_data[:make] = registration[:make]
+          vehicle_data[:model] = registration[:model_year]
+          vehicle_data[:engine] = :ev
+          Vehicle.new(vehicle_data)
+        end
+      end
 end
