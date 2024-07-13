270977366386d0b1eeeecc7bac258fce177ef430
Jim Macur <jim.macur@gmail.com>
2024-07-13 17:23:10 -0600

    creates cleaner looking code

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,17 +1,16 @@
 class VehicleFactory
-    
-    def initialize 
+    def initialize
     end
-
+  
     def create_vehicles(dmv_registrations)
-        dmv_registrations.map do |registration|
-          vehicle_data = {}
-          vehicle_data[:vin] = registration[:vin_1_10]
-          vehicle_data[:year] = registration[:model_year]
-          vehicle_data[:make] = registration[:make]
-          vehicle_data[:model] = registration[:model_year]
-          vehicle_data[:engine] = :ev
-          Vehicle.new(vehicle_data)
-        end
+      dmv_registrations.map do |registration|
+        Vehicle.new(
+          vin: registration[:vin_1_10],
+          year: registration[:model_year],
+          make: registration[:make],
+          model: registration[:model],
+          engine: :ev
+        )
       end
-end
+    end
+  end
