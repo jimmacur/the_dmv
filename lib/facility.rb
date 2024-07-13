91d3b68884547a3165da2bd56f1a25b8296c1532
Jim Macur <jim.macur@gmail.com>
2024-07-13 14:19:16 -0600

    Makes facility class more efficient

--- lib/facility.rb
+++ lib/facility.rb
@@ -6,7 +6,6 @@ class Facility
               :registered_vehicles,
               :collected_fees
 
-
   def initialize(info)
     @name = info[:name]
     @address = info[:address]
@@ -49,16 +48,14 @@ class Facility
   end
 
   def administer_written_test(registrant)
-    return false unless registrant.age > 15
-    return false unless registrant.permit? == true
+    return false unless registrant.age > 15 && registrant.permit?
     allow_service('Written Test') do
       registrant.license_data[:written] = true
     end
   end
 
   def  administer_road_test(registrant)
-    return false unless registrant.permit?
-    return false unless registrant.license_data[:written]
+    return false unless registrant.permit? && registrant.license_data[:written]
     allow_service('Road Test') do
       registrant.license_data[:license] = true
       true
@@ -68,7 +65,6 @@ class Facility
   def renew_drivers_license(registrant)
     return false unless registrant.license_data[:license]
     allow_service('Renew License') do
-      registrant.license_data[:license] = true
       registrant.license_data[:renewed] = true
       true
     end
